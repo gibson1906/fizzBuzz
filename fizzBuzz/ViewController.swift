a98b5c93d52e0e9af2bd74f1573e3ae24cf51898
Tim Gibson <timgibson@Tims-MacBook-Pro.local>
2015-10-26 11:46:28 -0400

    Sending code for Fizz Buzz project (commented)

--- fizzBuzz/ViewController.swift
+++ fizzBuzz/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    for number in 1...100{
+//    
+//    let fizz = (number % 3)
+//    let buzz = (number % 5)
+//    
+//    if fizz == 0 && buzz == 0{
+//        print("fizz buzz")
+//    } else if fizz == 0{
+//        print("fizz")
+//    } else if buzz == 0{
+//            print("buzz")
+//    } else if number == number{
+//        print (number)}
+//        }
 
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
